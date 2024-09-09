ccdf6fbd164f6a611ff1c880b5cb9d0f07804405
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-09 10:44:29 -0500

    FX, Vercel test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "https://kit.fontawesome.com/2e0b382a53.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
