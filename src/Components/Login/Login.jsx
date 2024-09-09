9995f2b0b4174608bd40f705545ed92ef3d0b24f
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-09 09:57:21 -0500

    FX, Versel test

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Login.module.css";
-import Header from "../Header/Header1";
-import Footer from "../Footer/Footer";
-import LoginForm from "./LoginForm";
+import Header from "../Header/Header1.jsx";
+import Footer from "../Footer/Footer.jsx";
+import LoginForm from "./LoginForm.jsx";
 
 const Login = () => {
   return (
