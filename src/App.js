6e08b9daed75b417b981fb2782a9025b55058ce3
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-09 11:00:10 -0500

    FX, Vercel test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./Components/homepage/HomePage";
-import Login from "./Components/login/Login";
+import HomePage from "./Components/HomePage/HomePage";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <Routes>
       {/* Ruta para la página de inicio */}
       <Route path="/" element={<HomePage />} />
+
       {/* Ruta para la página de login */}
       <Route path="/login" element={<Login />} />
     </Routes>
