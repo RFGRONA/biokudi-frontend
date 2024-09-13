c436a5f8025b98e2ccb29480bfa5a6d92274b8f0
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-13 15:45:11 -0500

    RF, App.js
    Como las funciones de redireccionamiento de rutas se hacen en el index.js, no necesitamos el uso de App, a no ser que haya alguna plantilla

--- src/App.js
+++ src/App.js
@@ -3,16 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
 import Login from "./Components/Login/Login";
 
-function App() {
-  return (
-    <Routes>
-      {/* Ruta para la página de inicio */}
-      <Route path="/" element={<HomePage />} />
-
-      {/* Ruta para la página de login */}
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  );
-}
+function App() {}
 
 export default App;
