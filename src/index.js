7c1c059cc7ca802f2c56a145f75267f1ec4349d2
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-13 15:45:47 -0500

    VW, Error view added
    Vista de error añadida, con su respectivo redireccionamiento

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "https://kit.fontawesome.com/2e0b382a53.js";
 import { AuthProvider } from "./context/AuthContext";
+
+/**Routes */
 import Login from "./Components/Login/Login";
 import HomePage from "./Components/HomePage/HomePage";
+import Register from "./Components/register/Register";
+import Error from "./Components/error/Error";
 
 /*React Router */
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import PrivateRoute from "./Components/PrivateRoute";
 import PublicRoute from "./Components/PublicRoute";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -24,6 +28,8 @@ root.render(
           <Route element={<PublicRoute />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Error />} />
           </Route>
 
           {/* Rutas privadas, necesitan auth*/}
