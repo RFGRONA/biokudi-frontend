c81cc7cd91ea49f35df93cc252096c2770cee89d
Kevinz0317 <127569355+Kevinz0317@users.noreply.github.com>
2024-09-14 07:42:15 -0500

    CF - Paths added
    Se agregaron las rutas asociadas a: Politica de Privacidad, Cookies, Legal y Sobre Nosotros

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
 import Login from "./Components/Login/Login";
+import PrivacyPolicy from "./Components/policies/PrivacyPolicy"
+import Cookies from "./Components/policies/Cookies";
+import Legal from "./Components/policies/Legal";
+import AboutUs from "./Components/policies/AboutUs";
 
 function App() {
   return (
@@ -11,6 +15,18 @@ function App() {
 
       {/* Ruta para la página de login */}
       <Route path="/login" element={<Login />} />
+
+      {/* Ruta para la página de politica de privacidad */}
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+      {/* Ruta para la página de cookies */}
+      <Route path="/cookies" element={<Cookies />} />
+
+      {/* Ruta para la página de legal */}
+      <Route path="/legal" element={<Legal />} />
+
+      {/* Ruta para la página de sobre nosotros */}
+      <Route path="/about-us" element={<AboutUs />} />
     </Routes>
   );
 }
