22eb697a6c16a0e03a16969cabd808ca22c1a3cc
IvoneLopez2407 <iglopez@ucundinamarca.edu.co>
2024-09-13 20:10:16 -0500

    VW

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage";
 import Login from "./Components/Login/Login";
 
 function App() {
   return (
-    <Routes>
-      {/* Ruta para la página de inicio */}
-      <Route path="/" element={<HomePage />} />
-
-      {/* Ruta para la página de login */}
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
