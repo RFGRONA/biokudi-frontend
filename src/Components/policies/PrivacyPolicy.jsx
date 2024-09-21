a8440b912c4aa7ee4e7d052d866a89f2a49db77e
Kevinz0317 <127569355+Kevinz0317@users.noreply.github.com>
2024-09-20 20:20:48 -0500

    MN-VW - Privacy Policy View
    Se realizó un cambio en el diseño del componente.

--- src/Components/policies/PrivacyPolicy.jsx
+++ src/Components/policies/PrivacyPolicy.jsx
@@ -8,10 +8,7 @@ const PrivacyPolicy = () => {
       <p>Bienvenido a nuestro sitio web dedicado al ecoturismo en Cundinamarca. La privacidad de nuestros usuarios es de suma importancia para nosotros. Esta Política de Privacidad explica cómo recopilamos, utilizamos, y protegemos la información personal que obtenemos a través de nuestro sitio web.</p>
       <p>Cuando los usuarios se registran en nuestro sitio web, dejan comentarios o reseñas, recopilamos información personal que puede incluir datos de contacto. No vendemos ni compartimos la información personal de nuestros usuarios con terceros no relacionados.</p>
       <p>Implementamos medidas de seguridad adecuadas para proteger la información personal contra acceso no autorizado, alteración, divulgación o destrucción.</p>
-      <p>
-      Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Cualquier cambio será publicado en esta página 
-      </p>
-
+      <p>Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Cualquier cambio será publicado en esta página </p>
     </Layout>
   );
 };
