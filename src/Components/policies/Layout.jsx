67412444ca70b5983ada8d7d2af98a22bbe58edf
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-23 12:02:58 -0500

    MN, View fixed

--- src/Components/policies/Layout.jsx
+++ src/Components/policies/Layout.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header1 from '../header/Header1';
-import Footer from '../footer/Footer';
-import styles from './Layout.module.css'
+import React from "react";
+import Header from "../header/Header2";
+import Footer from "../footer/Footer";
+import styles from "./Layout.module.css";
 
 const Layout = ({ icon, title, children }) => {
   const addClassToParagraphs = (children) => {
     return React.Children.map(children, (child) => {
-      if (React.isValidElement(child) && child.type === 'p') {
+      if (React.isValidElement(child) && child.type === "p") {
         return React.cloneElement(child, { className: styles.layoutPara });
       }
       return child;
@@ -14,15 +14,19 @@ const Layout = ({ icon, title, children }) => {
   };
   return (
     <div>
-      <Header1 />
-    <div className={styles.layoutContainer}>
-          <main className={styles.layoutBody}>
-          <h1>{icon && <img src={icon} alt="Icono" className={styles.icon} />}{title}</h1>
-          <hr></hr><br></br>
-            {addClassToParagraphs(children)}
-          </main>
-    </div>
-        <Footer />
+      <Header />
+      <div className={styles.layoutContainer}>
+        <main className={styles.layoutBody}>
+          <h1>
+            {icon && <img src={icon} alt="Icono" className={styles.icon} />}
+            {title}
+          </h1>
+          <hr></hr>
+          <br></br>
+          {addClassToParagraphs(children)}
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 };
