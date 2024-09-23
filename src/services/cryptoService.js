d6b74a6aa76f132cad8a26196c36b6d52f7fe4ba
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-23 18:47:54 -0500

    FX, crypto service

--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -7,8 +7,9 @@ const fetchPublicKey = async () => {
   const URL_PUBLIC_KEY = process.env.REACT_APP_URL_API + "/api/public-key";
   try {
     const response = await axios.get(URL_PUBLIC_KEY);
-    if (response.status === 200) {
-      publicKey = response.data;
+    if (response.status === 200 && response.data) {
+      publicKey = response.data.trim(); // Asegúrate de eliminar espacios en blanco
+      console.log("Clave pública obtenida:", publicKey);
     } else {
       throw new Error("No se pudo obtener la clave pública.");
     }
@@ -32,13 +33,15 @@ const encryptPassword = async (password) => {
   const passwordBuffer = encoder.encode(password);
 
   try {
+    const keyBuffer = pemToArrayBuffer(publicKey);
+    
     // Importa la clave pública usando RSA-OAEP con SHA-256
     const importedKey = await window.crypto.subtle.importKey(
       "spki",
-      pemToArrayBuffer(publicKey),
+      keyBuffer,
       {
         name: "RSA-OAEP",
-        hash: { name: "SHA-256" }, // Cambiado a SHA-256
+        hash: { name: "SHA-256" },
       },
       false,
       ["encrypt"]
@@ -57,6 +60,8 @@ const encryptPassword = async (password) => {
     return arrayBufferToBase64(encryptedPasswordBuffer);
   } catch (error) {
     console.error("Error al encriptar la contraseña:", error);
+    console.error("Clave pública:", publicKey);
+    console.error("Contraseña:", password);
     throw error;
   }
 };
@@ -66,16 +71,19 @@ const pemToArrayBuffer = (pem) => {
   if (!pem) {
     throw new Error("La clave PEM proporcionada es inválida.");
   }
-  const b64Lines = pem.replace(/-----(BEGIN|END) PUBLIC KEY-----/g, "").trim();
-  const b64 = b64Lines.replace(/[\r\n]+/g, "");
+  
+  // Elimina los saltos de línea y espacios en blanco
+  const b64 = pem.replace(/-----BEGIN PUBLIC KEY-----|-----END PUBLIC KEY-----|\s/g, '');
+  
   try {
-    const binaryString = atob(b64);
-    const binaryArray = new Uint8Array(binaryString.length);
-    for (let i = 0; i < binaryString.length; i++) {
-      binaryArray[i] = binaryString.charCodeAt(i);
+    const binary = atob(b64);
+    const bytes = new Uint8Array(binary.length);
+    for (let i = 0; i < binary.length; i++) {
+      bytes[i] = binary.charCodeAt(i);
     }
-    return binaryArray.buffer;
+    return bytes.buffer;
   } catch (error) {
+    console.error("Error al decodificar la clave pública:", error);
     throw new Error("Error al decodificar la clave pública en Base64.");
   }
 };
