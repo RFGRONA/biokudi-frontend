f10192972b953289fa3c2c211eddad3f5fdc8207
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-25 12:32:39 -0500

    MC, import unnecesary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./Components/HomePage/HomePage";
-import Login from "./Components/Login/Login";
-import PrivacyPolicy from "./Components/policies/PrivacyPolicy";
-import Cookies from "./Components/policies/Cookies";
-import Legal from "./Components/policies/Legal";
-import AboutUs from "./Components/policies/AboutUs";
-import Help from "./Components/policies/Help";
-import Contact from "./Components/policies/Contact";
 
 function App() {}
 
