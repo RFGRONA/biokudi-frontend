0f570da60a315b5138c7816f0184a7832c988cea
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-09-26 21:25:00 -0500

    MN, Unnecesary function

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,11 +7,14 @@ export const loginApi = async (email, password, rememberme, captchatoken) => {
     const { encryptPassword } = CryptoService;
     const passwordEncrypted = await encryptPassword(password);
 
-    const response = await axios.post(URL_LOGIN, {
-      email,
-      password: passwordEncrypted,
-      rememberme,
-      captchatoken},
+    const response = await axios.post(
+      URL_LOGIN,
+      {
+        email,
+        password: passwordEncrypted,
+        rememberme,
+        captchatoken,
+      },
       { withCredentials: true }
     );
     if (response.status === 200) {
@@ -24,28 +27,5 @@ export const loginApi = async (email, password, rememberme, captchatoken) => {
     const { response } = error;
     console.log("Error en la autenticación", response);
     return response;
-  }
-};
-
-export const verifyTokenApi = async (jwtToken) => {
-  try {
-    if (!jwtToken) {
-      const response = { status: 401 };
-      return response;
-    }
-    const response = axios.post(
-      process.env.REACT_APP_URL_API + "/auth/check-session",
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${jwtToken}`,
-        },
-      }
-    );
-    console.log("Respuesta: ", response);
-    return response;
-  } catch (error) {
-    console.error("Error verificando token:", error);
-    return error;
   }
 };
