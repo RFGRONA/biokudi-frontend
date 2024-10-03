572fbdd4f089fd52e8a6e8260eb95e03372cfc6a
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-10-03 18:16:01 -0500

    FX, user context for session

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,91 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 import { loginApi } from "../services/authService";
+import { useLocation } from "react-router-dom";
+import CryptoJS from "crypto-js"; // Importar la librería para cifrado
 
 const AuthContext = createContext();
+let authChecked = false; // Usado para evitar múltiples solicitudes al backend
+
+const SECRET_KEY = process.env.REACT_APP_SESSION_KEY; // Clave para cifrar y descifrar
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Estado del usuario
-  const [loading, setLoading] = useState(false); // Estado de carga
-
-  // Función para cargar el usuario desde localStorage al montar el componente
-  useEffect(() => {
-    const storedUser = localStorage.getItem("userData");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
+  const [user, setUser] = useState(() => {
+    // Restaurar los datos del usuario desde sessionStorage si están presentes y descifrarlos
+    const encryptedUser = sessionStorage.getItem("user");
+    if (encryptedUser) {
+      try {
+        const bytes = CryptoJS.AES.decrypt(encryptedUser, SECRET_KEY);
+        const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+        return decryptedData;
+      } catch (error) {
+        console.error("Error al descifrar los datos del usuario", error);
+        return null;
+      }
     }
-  }, []);
+    return null;
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [firstLoad, setFirstLoad] = useState(true);
+  const location = useLocation();
+
+  // Función para verificar la sesión
+  const checkAuth = async () => {
+    if (authChecked) return; // Si ya se verificó la sesión, no la verifiques de nuevo
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        process.env.REACT_APP_URL_API + "/auth/check-session",
+        { withCredentials: true }
+      );
+      const { data } = response;
+
+      const userData = {
+        name: data.nameUser,
+        id: data.userId,
+        email: data.email,
+        role: data.role,
+        photo: data.profilePicture,
+      };
+      setUser(userData);
+
+      // Cifrar los datos del usuario antes de almacenarlos en sessionStorage
+      const encryptedData = CryptoJS.AES.encrypt(
+        JSON.stringify(userData),
+        SECRET_KEY
+      ).toString();
+      sessionStorage.setItem("user", encryptedData);
+      authChecked = true; // Marcar que ya hemos verificado la autenticación
+    } catch (error) {
+      setUser(null);
+      sessionStorage.removeItem("user"); // Limpiar datos del usuario si falla la autenticación
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Función para restaurar la sesión si el usuario eligió recordarla
+  useEffect(() => {
+    const restoreSession = async () => {
+      const rememberedSession = localStorage.getItem("rememberSession");
+
+      // Solo restaurar la sesión si la flag persistente (localStorage) está activa
+      if (firstLoad && rememberedSession === "true") {
+        if (!user) {
+          // Solo hacemos checkAuth si no hay un usuario almacenado
+          await checkAuth();
+        }
+      }
+
+      // Una vez hecho el primer check, cambiamos firstLoad a false
+      setFirstLoad(false);
+    };
+
+    if (firstLoad) {
+      restoreSession();
+    }
+  }, [firstLoad, user]);
 
   // Función para hacer login
   const login = async (email, password, remember, captchaToken) => {
@@ -30,15 +101,17 @@ export const AuthProvider = ({ children }) => {
           email: data.email,
           role: data.role,
           photo: data.profilePicture,
-          phone: data?.phone,
         };
-
         setUser(userData);
 
-        // Almacenar datos del usuario en localStorage
-        localStorage.setItem("userData", JSON.stringify(userData));
+        // Cifrar los datos del usuario antes de almacenarlos en sessionStorage
+        const encryptedData = CryptoJS.AES.encrypt(
+          JSON.stringify(userData),
+          SECRET_KEY
+        ).toString();
+        sessionStorage.setItem("user", encryptedData);
 
-        // Guardar indicador de sesión recordada
+        // Guardar en localStorage si el usuario decidió recordar la sesión
         if (remember) {
           localStorage.setItem("rememberSession", "true");
         } else {
@@ -65,8 +138,9 @@ export const AuthProvider = ({ children }) => {
         { withCredentials: true }
       );
       setUser(null);
-      localStorage.removeItem("userData");
+      sessionStorage.removeItem("user");
       localStorage.removeItem("rememberSession");
+      authChecked = false; // Resetear para permitir la verificación de autenticación nuevamente
     } catch (error) {
       console.error("Error durante logout", error);
     }
